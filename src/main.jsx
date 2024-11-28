f52fc62f5129dc92266dc43d9483fa67f11ed00f
toanla05 <toanthienla2005@gmail.com>
2024-11-28 15:16:02 +0700

    Fix flickering when dragging card with image  to other column

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
@@ -6,10 +5,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </CssVarsProvider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <CssVarsProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </CssVarsProvider>
+  // </React.StrictMode>
 );
