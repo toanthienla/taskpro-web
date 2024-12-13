5bb46e766d8d6e0d5acc0753bace6c8a1fee315b
toanla05 <toanthienla2005@gmail.com>
2024-12-13 18:26:59 +0700

    Add remove column function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Board from './pages/Boards/_id';
+import { ConfirmProvider } from 'material-ui-confirm';
 
 function App() {
 
   return (
-    <>
+    <ConfirmProvider>
       <Board></Board>
-    </>
+    </ConfirmProvider>
   );
 }
 
