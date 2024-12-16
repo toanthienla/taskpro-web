edc7b66e3656a8811c13908fcefff33f98b625a0
toanla05 <toanthienla2005@gmail.com>
2024-12-16 13:33:04 +0700

    Setup basic Redux toolkit store and redux middleware api

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme.js';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from 'react-redux';
+import { store } from '~/redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <CssVarsProvider theme={theme}>
-    <CssBaseline />
-    <App />
-    <ToastContainer pauseOnFocusLoss={false} newestOnTop />
-  </CssVarsProvider>
-  // </React.StrictMode>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+      <ToastContainer pauseOnFocusLoss={false} newestOnTop />
+    </CssVarsProvider>
+  </Provider>
 );
---
+++ src/redux/activeBoard/activeBoardSlice.js
@@ -0,0 +1,70 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { API_ROOT } from '~/utils/constants';
+import { isEmpty } from 'lodash';
+import { generatePlaceholderCard } from '~/utils/formatters';
+import { mapOrder } from '~/utils/sorts';
+
+const initialState = {
+  currentActiveBoard: null
+};
+
+// Async function when call API
+export const getBoardApi = createAsyncThunk(
+  'activeBoardSlice/getBoardApi',
+  async (boardId) => {
+    const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`);
+    return response.data;
+  }
+);
+
+export const activeBoardSlice = createSlice({
+  name: 'activeBoard',
+  initialState,
+  reducers: {
+    updateCurrentActiveBoard: (state, action) => {
+      let board = action.payload;
+
+      // Sort column by columnOrderIds
+      board.columns = mapOrder(board?.columns, board?.columnOrderIds, '_id');
+
+      board.columns.forEach(column => {
+        // Add placeholder card if column have empty cards array
+        if (isEmpty(column?.cards)) {
+          const placeholderCard = generatePlaceholderCard(column);
+          column.cardOrderIds.push(placeholderCard._id);
+          column.cards.push(placeholderCard);
+        } else {
+          // Sort card by cardOrderIds
+          column.cards = mapOrder(column?.cards, column?.cardOrderIds, '_id');
+        }
+      });
+
+      state.currentActiveBoard = board;
+    }
+  },
+  // Handle async data
+  extraReducers: (builder) => {
+    builder.addCase(getBoardApi.fulfilled, (state, action) => {
+      // fulfilled is when promise is success
+      // action.payload is return reponse data
+      const board = action.payload;
+
+      // Perform logic if need
+      // ...
+
+      state.currentActiveBoard = board;
+    });
+  }
+});
+
+// Action creators are generated for each case reducer function
+// dispath() -> update data by reducer
+export const { updateCurrentActiveBoard } = activeBoardSlice.actions;
+
+// Selectors -> useSelector()
+export const selectCurrentActiveBoard = (state) => {
+  return state.activeBoard.currentActiveBoard;
+};
+
+export const activeBoardReducer = activeBoardSlice.reducer;
