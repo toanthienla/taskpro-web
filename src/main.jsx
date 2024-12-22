677fb544e462cf66af146df1b4449fab5bb4abf3
toanla05 <toanthienla2005@gmail.com>
2024-12-22 23:04:06 +0700

    Update displayname and password settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import { GlobalStyles } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme.js';
 import { ToastContainer, Bounce } from 'react-toastify';
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <PersistGate persistor={persistor}>
         <CssVarsProvider theme={theme}>
           <ConfirmProvider>
+            <GlobalStyles styles={{ a: { textDecoration: 'none' } }} />
             <CssBaseline />
             <App />
             <ToastContainer
